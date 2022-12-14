61eaed50c2bc51e800876018d9ee801451cf871f
Lelya2210 <117176572+Lelya2210@users.noreply.github.com>
2022-12-14 17:26:13 +0300

    Update first_test.py

--- tests/first_test.py
+++ tests/first_test.py
@@ -8,7 +8,12 @@ class TestCalculator:
     def test_multyply_calculator_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
- #   def test_multyply_calculator_failed(self):
- #       assert self.calc.multiply(self, 2, 2) == 5
+    def test_division_calculator_correctly(self):
+        assert self.calc.division(self, 24, 8) == 3
 
+    def test_subtraction_calculator_correctly(self):
+        assert self.calc.subtraction(self, 24, 8) == 16
+
+    def test_adding_calculator_correctly(self):
+        assert self.calc.adding(self, 5, 3) == 8
  
